0cc6cc6ef186d1799a9921873282158b39e49b49
RishavNeupane <rishavneuane98@gmail.com>
2026-02-04 10:58:00 +0545

    Codi AI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import Learn from "./pages/Learn";
+
+const App = () => {
+    return (
+        <>
+            <Home />
+            <Learn />
+        </>
+    );
+}
+export default App;
